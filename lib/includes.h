518c501c8b8ad95ed83eef0b8a6d2c28ded2199f
Dmitry Sutygin <demonscorpion@yandex.ru>
2016-03-17 12:42:16 +0400

    added includes in generated cpp and namespace solution scope

---
+++ lib/includes.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <sstream>
+
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <queue>
+#include <deque>
+#include <stack>
+
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <string.h>
+
+#include <memory.h>
+#include <cassert>
+#include <time.h>
--- src/compiler/compiler.py
+++ src/compiler/compiler.py
@@ -25,10 +25,30 @@ def get_output_file_name(settings):
     return settings['o']
 
 
+def write_includes(output_file):
+    with open('lib/includes.h', 'r') as includes:
+        output_file.writelines(includes.readlines())
+        output_file.write('\n')
+
+
+def write_main_function(cpp_file):
+    main_function = [
+        'int main() {\n',
+        '  solution::main();\n',
+        '  return 0;\n',
+        '}\n',
+    ]
+    cpp_file.writelines(main_function)
+
+
 def generate_cpp(ast, output_file):
     with open(output_file, 'w', encoding='utf-8') as cpp_file:
+        write_includes(cpp_file)
+        cpp_file.write('namespace solution {\n\n')
         visitor = print_visitor.PrintVisitor(cpp_file)
         ast.accept(visitor)
+        cpp_file.write('\n}\n\n')
+        write_main_function(cpp_file)
 
 
 def main():
