851c6457e090fdc369773e148aebcf9d93cf76e2
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2016-05-05 08:01:19 +0400

    added implementation of SymbolTable

--- src/visitors/semantic_analysis/symbol_table.py
+++ src/visitors/semantic_analysis/symbol_table.py
@@ -1,18 +1,22 @@
 class SymbolTable:
     def __init__(self):
-        pass
+        self._variables = {}
+        self._functions = {}
+        self._types = {}
 
     def insert_variable(self, variable):
-        pass
+        self._variables[variable.identifier] = variable
 
     def insert_function(self, function):
-        pass
+        if self._functions.get(function.identifier) is None:
+            self._functions[function.identifier] = []
+        self._functions[function.identifier].append(function)
 
-    def insert_new_type(self, variable_type):
-        pass
+    def insert_new_type(self, identifier, symbol_table):
+        self._types[identifier] = symbol_table
 
     def lookup_variable(self, identifier):
-        pass
+        return self._variables.get(identifier)
 
     def lookup_function(self, function):
         pass
