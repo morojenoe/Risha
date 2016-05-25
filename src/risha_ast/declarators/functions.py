59bcea528c3824ce5059fb2e26b18ee923b255b1
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2016-05-25 09:20:25 +0400

    added before-after accept method to functions.py

--- src/risha_ast/declarators/functions.py
+++ src/risha_ast/declarators/functions.py
@@ -9,6 +9,12 @@ class FunctionDefinition(ASTNode):
     def accept_print_visitor(self, visitor):
         visitor.visit_function_definition_before(self)
 
+    def accept_before_after(self, visitor):
+        visitor.visit_function_definition_before(self)
+        self.declarator_with_specifiers.accept_before_after(visitor)
+        self.body.accept_before_after(visitor)
+        visitor.visit_function_definition_after(self)
+
 
 class ParameterDeclarationList(CommaSeparatedList):
     pass
@@ -21,3 +27,10 @@ class ParameterDeclaration(ASTNode):
 
     def accept_print_visitor(self, visitor):
         visitor.visit_param_decl_before(self)
+
+    def accept_before_after(self, visitor):
+        visitor.visit_param_decl_before(self)
+        self.declarator_with_specifiers.accept_before_after(visitor)
+        if self.initializer is not None:
+            self.initializer.accept_before_after(visitor)
+        visitor.visit_param_decl_after(self)
