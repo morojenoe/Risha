93ce19f1e5e47db4fb67a0a2e4d7503f204891a3
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2016-05-26 09:23:26 +0400

    added "num_new_lines_after_decl" parameter to risha_ast.Program

--- src/risha_ast/program.py
+++ src/risha_ast/program.py
@@ -2,11 +2,12 @@ from . import ASTNode
 
 
 class Program(ASTNode):
-    def __init__(self, declarations=None):
-        self.declarations = declarations if declarations is not None else []
+    def __init__(self, declarations=None, num_new_lines_after_decl=2):
+        self._declarations = declarations if declarations is not None else []
+        self._num_new_lines = num_new_lines_after_decl
 
     def add(self, declaration):
-        self.declarations.append(declaration)
+        self._declarations.append(declaration)
         return self
 
     def accept_print_visitor(self, visitor):
@@ -14,6 +15,13 @@ class Program(ASTNode):
 
     def accept_before_after(self, visitor):
         visitor.visit_program_before(self)
-        for declaration in self.declarations:
+        for declaration in self._declarations:
             declaration.accept_before_after(visitor)
         visitor.visit_program_after(self)
+
+    def get_new_lines(self):
+        return self._num_new_lines
+
+    def get_declarations(self):
+        for decl in self._declarations:
+            yield decl
