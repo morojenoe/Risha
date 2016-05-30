9d11cf822bbcf7fe11d16896307e567f6fbc9422
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2016-05-30 09:32:24 +0400

    refactoring: ParameterDeclaration fields are substituted by properties

--- src/risha_ast/declarators/functions.py
+++ src/risha_ast/declarators/functions.py
@@ -29,16 +29,24 @@ class ParameterDeclarationList(CommaSeparatedList):
 
 
 class ParameterDeclaration(ASTNode):
-    def __init__(self, declarator_with_specifiers, initializer):
-        self.declarator_with_specifiers = declarator_with_specifiers
-        self.initializer = initializer
+    def __init__(self, simple_declaration, initializer):
+        self._simple_declaration = simple_declaration
+        self._initializer = initializer
 
     def accept_print_visitor(self, visitor):
         visitor.visit_param_decl_before(self)
 
     def accept_before_after(self, visitor):
         visitor.visit_param_decl_before(self)
-        self.declarator_with_specifiers.accept_before_after(visitor)
-        if self.initializer is not None:
-            self.initializer.accept_before_after(visitor)
+        self._simple_declaration.accept_before_after(visitor)
+        if self._initializer is not None:
+            self._initializer.accept_before_after(visitor)
         visitor.visit_param_decl_after(self)
+
+    @property
+    def simple_declaration(self):
+        return self._simple_declaration
+
+    @property
+    def initializer(self):
+        return self._initializer
