f7783e98a5125aaac337f52cd9a96fe6d4c6a9b9
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2017-01-07 09:00:08 +0400

    updated data_walker class definition is erased from data

--- src/ast_visitors/pretty_printer/walkers/data_walker.py
+++ src/ast_visitors/pretty_printer/walkers/data_walker.py
@@ -7,12 +7,16 @@ class DataWalker(EmptyTreeWalker):
         super().__init__()
         self._data = []
         self._count_scope_enterings = 0
+        self._count_class_enterings = 0
 
     def visit_class_before(self, class_node):
         self._count_scope_enterings += 1
+        self._count_class_enterings += 1
 
     def visit_class_after(self, class_node):
         self._count_scope_enterings -= 1
+        if self._count_class_enterings > 1:
+            self._count_class_enterings -= 1
 
     def visit_function_definition_before(self, function_definition):
         self._count_scope_enterings += 1
@@ -20,9 +24,13 @@ class DataWalker(EmptyTreeWalker):
     def visit_function_definition_after(self, function_definition):
         self._count_scope_enterings -= 1
 
-    def visit_simple_declaration_before(self, simple_declaration):
-        if self._count_scope_enterings == 0:
+    def visit_simple_declaration_after(self, simple_declaration):
+        if (self._count_scope_enterings == 0 and
+                    self._count_class_enterings == 0):
             self._data.append(simple_declaration)
+        if (self._count_scope_enterings == 0 and
+                    self._count_class_enterings == 1):
+            self._count_class_enterings = 0
 
     def get_data(self):
         for data in self._data:
